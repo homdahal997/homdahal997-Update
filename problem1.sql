b8efc5e607f2a99be23e08c00e9299d2bd7593c7
homdahal997 <homdahal997@gmail.com>
2023-03-07 15:18:26 +0000

    updating particular column

---
+++ problem1.sql
@@ -0,0 +1,6 @@
+        /**
+         * Problem 1: Update Alexa's last name to be 'Rush' in the site_user table.
+         */
+         Update site_user
+         SET lastname = 'Rush'
+         WHERE firstname = 'Alexa'
